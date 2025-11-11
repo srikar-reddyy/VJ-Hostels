bb334378a5ed0fc98510d389e4c80db2067b1246
Gurram Karthik <167804249+gurramkarthiknetha@users.noreply.github.com>
2025-11-11 20:57:41 +0530

    fixed bookmark icon

--- frontend/src/components/admin/Students.jsx
+++ frontend/src/components/admin/Students.jsx
@@ -236,7 +236,10 @@ const Students = () => {
                         className={`btn ${viewMode === 'bookmarked' ? 'btn-warning' : 'btn-outline-warning'}`}
                         onClick={() => setViewMode('bookmarked')}
                     >
-                        ⭐ Bookmarked
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16" style={{ marginRight: '5px' }}>
+                            <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.777.416L8 13.101l-5.223 2.815A.5.5 0 0 1 2 15.5V2zm2-1a1 1 0 0 0-1 1v12.566l4.723-2.482a.5.5 0 0 1 .554 0L13 14.566V2a1 1 0 0 0-1-1H4z"/>
+                        </svg>
+                        Bookmarked
                     </button>
                     <button
                         className="btn btn-success"
@@ -478,7 +481,15 @@ const Students = () => {
                                                         onClick={() => handleToggleBookmark(student.rollNumber, student.isBookmarked)}
                                                         title={student.isBookmarked ? 'Remove bookmark' : 'Bookmark student'}
                                                     >
-                                                        {student.isBookmarked ? '⭐' : '☆'}
+                                                        {student.isBookmarked ? (
+                                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                                                                <path d="M2 2v13.5a.5.5 0 0 0 .74.439L8 13.069l5.26 2.87A.5.5 0 0 0 14 15.5V2a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2z"/>
+                                                            </svg>
+                                                        ) : (
+                                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                                                                <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.777.416L8 13.101l-5.223 2.815A.5.5 0 0 1 2 15.5V2zm2-1a1 1 0 0 0-1 1v12.566l4.723-2.482a.5.5 0 0 1 .554 0L13 14.566V2a1 1 0 0 0-1-1H4z"/>
+                                                            </svg>
+                                                        )}
                                                     </button>
                                                     <button
                                                         className="btn btn-sm btn-info"
